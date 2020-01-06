48a396be5d25b55ba88244774e7d7318db6d9053
syler023 <gen23.al@gmail.com>
2020-01-06 14:58:38 -0400

    adding conection via proxy of compute engine and sql

--- startup_script.bash
+++ startup_script.bash
@@ -8,3 +8,5 @@ git clone https://github.com/stashconsulting/pythondemo.git
 cd ./pythondemo/
 pip install -r requirements.txt
 python app.py &
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
