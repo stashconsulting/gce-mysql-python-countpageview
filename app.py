e7f9cde8ca9033607424d043fabc706037d5a0c6
syler023 <gen23.al@gmail.com>
2020-01-06 15:58:54 -0400

    adding pymsql

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import pymysql
+
+conn = pymysql.connect(
+    host="localhost", port=3306, user="root",
+    passwd="i234", db="page_view"
+)
 
 app = Flask(__name__)
 api = Api(app)
