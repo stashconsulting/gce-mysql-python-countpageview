6a5a274819450adb42ae2acb1e9183a1aca71e9b
syler023 <gen23.al@gmail.com>
2020-01-06 21:09:31 -0400

    adding the proxy in startup_ script, and the count view of the app

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from os import environ
 import pymysql
 
 conn = pymysql.connect(
-    host="localhost", port=3306, user="root",
-    passwd="i234", db="page_view"
+    host=environ.get('host'), port=environ.get('port',3306), user=environ.get('user'),
+    passwd=environ.get('passwd'), db=environ.get('db')
 )
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT count_view FROM view"
+        )
+        result =  cursor.fetchone()
+        if result is None:
+            cursor.execute(
+            "INSERT INTO view VALUES (1)"
+            )
+        else:
+            result = result[0] + 1
+            cursor.execute(
+            "UPDATE view set count_view = {}".format(result)
+            )
+
+        conn.commit()
+        return result      
 
 api.add_resource(HelloWorld, '/')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
--- startup_script.bash
+++ startup_script.bash
@@ -4,9 +4,10 @@ sudo apt install python-pip -y
 sudo apt install git -y
 mkdir /app
 cd /app
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+./cloud_sql_proxy -instances="stone-door-258502:us-west1:instance"=tcp:3306
 git clone https://github.com/stashconsulting/pythondemo.git
 cd ./pythondemo/
 pip install -r requirements.txt
 python app.py &
-wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
-chmod +x cloud_sql_proxy
